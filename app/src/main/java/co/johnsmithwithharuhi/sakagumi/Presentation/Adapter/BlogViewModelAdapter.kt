d675a8a14c12b86f7f4a2494011f0e02107bc72b
JohnSmithWithHaruhi <JohnSmithWithHaruhi@users.noreply.github.com>
2017-08-15 21:14:45 +0900

    add BlogViewModelAdapter

---
+++ app/src/main/java/co/johnsmithwithharuhi/sakagumi/Presentation/Adapter/BlogViewModelAdapter.kt
@@ -0,0 +1,30 @@
+package co.johnsmithwithharuhi.sakagumi.Presentation.Adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import co.johnsmithwithharuhi.sakagumi.databinding.ItemBlogListBinding
+
+class BlogListAdapter : RecyclerView.Adapter<BlogListAdapter.ViewHolder>() {
+
+  override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+    TODO(
+        "not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun getItemCount(): Int {
+    TODO(
+        "not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    TODO(
+        "not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    private val mBinding: ItemBlogListBinding? = null
+
+  }
+
+}
