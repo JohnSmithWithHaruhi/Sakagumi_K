4f02d4743d3989a826bbc15b08458fd447426604
JohnSmithWithHaruhi <JohnSmithWithHaruhi@users.noreply.github.com>
2018-05-18 13:08:18 +0900

    code refactoring

--- app/src/main/java/co/johnsmithwithharuhi/sakagumi/Data/JSoup/BlogJSoup.kt
+++ app/src/main/java/co/johnsmithwithharuhi/sakagumi/Data/JSoup/BlogJSoup.kt
@@ -5,36 +5,30 @@ import org.jsoup.Jsoup
 import java.io.IOException
 import java.util.ArrayList
 
+private const val URL_NOG = "http://blog.nogizaka46.com"
+private const val URL_KEY = "http://www.keyakizaka46.com"
+private const val URL_KEY_OPTION = "/s/k46o/diary/member/list?ima=0000&page=0&rw=30&cd=member"
+
 class BlogJSoup {
-  private val URL_NOG = "http://blog.nogizaka46.com"
-  private val URL_KEY = "http://www.keyakizaka46.com"
 
   @Throws(IOException::class)
   fun createOsuBlogList(): List<Blog> {
     val blogList = ArrayList<Blog>()
-    val document = Jsoup.connect(
-        URL_KEY + "/s/k46o/diary/member/list?ima=0000&page=0&rw=30&cd=member&ct=07"
-    )
+    val document = Jsoup.connect("$URL_KEY$URL_KEY_OPTION&ct=07")
         .get()
     val elements = document.getElementsByTag("article")
     for (element in elements) {
-      val blog = Blog()
-      blog.type = Blog.KEY_OSU
-      blog.name = element.getElementsByClass("name")
-          .text()
-      blog.title = element.getElementsByTag("a")
-          .first()
-          .text()
-      blog.url = URL_KEY + element.getElementsByTag("a").first().attr("href")
-      blog.content = element.getElementsByClass("box-article")
-          .text()
-      blog.time =
-          element.getElementsByClass("box-bottom")
-              .first()
-              .getElementsByTag("li")
-              .first()
-              .text()
-      blogList.add(blog)
+      blogList.add(
+          Blog(
+              type = Blog.KEY_OSU,
+              name = element.getElementsByClass("name").text(),
+              title = element.getElementsByTag("a").first().text(),
+              url = URL_KEY + element.getElementsByTag("a").first().attr("href"),
+              content = element.getElementsByClass("box-article").text(),
+              time = element.getElementsByClass("box-bottom").first()
+                  .getElementsByTag("li").first().text()
+          )
+      )
     }
     return blogList
   }
@@ -42,28 +36,19 @@ class BlogJSoup {
   @Throws(IOException::class)
   fun createKeyBlogList(): List<Blog> {
     val blogList = ArrayList<Blog>()
-    val document = Jsoup.connect(
-        URL_KEY + "/s/k46o/diary/member/list?ima=0000&page=0&rw=30&cd=member"
-    )
+    val document = Jsoup.connect(URL_KEY + URL_KEY_OPTION)
         .get()
     val elements = document.getElementsByTag("article")
     for (element in elements) {
-      val blog = Blog()
-      blog.type = Blog.KEY_KEY
-      blog.name = element.getElementsByClass("name")
-          .text()
-      blog.title = element.getElementsByTag("a")
-          .first()
-          .text()
-      blog.url = URL_KEY + element.getElementsByTag("a").first().attr("href")
-      blog.content = element.getElementsByClass("box-article")
-          .text()
-      blog.time =
-          element.getElementsByClass("box-bottom")
-              .first()
-              .getElementsByTag("li")
-              .first()
-              .text()
+      val blog = Blog(
+          type = Blog.KEY_KEY,
+          name = element.getElementsByClass("name").text(),
+          title = element.getElementsByTag("a").first().text(),
+          url = URL_KEY + element.getElementsByTag("a").first().attr("href"),
+          content = element.getElementsByClass("box-article").text(),
+          time = element.getElementsByClass("box-bottom").first()
+              .getElementsByTag("li").first().text()
+      )
       blogList.add(blog)
     }
     return blogList
@@ -80,7 +65,7 @@ class BlogJSoup {
 
   @Throws(IOException::class)
   private fun createNogBlogList(page: Int): List<Blog> {
-    val document = Jsoup.connect(URL_NOG + "/?p=" + page)
+    val document = Jsoup.connect("$URL_NOG/?p=$page")
         .get()
     val headElements = document.select(".heading")
     val bodyElements = document.getElementsByClass("entrybody")
@@ -88,19 +73,14 @@ class BlogJSoup {
     val blogList = ArrayList<Blog>()
     for (i in 0 until headElements.size) {
       val headElement = headElements[i]
-      val blog = Blog()
-      blog.type = Blog.KEY_NOG
-      blog.name = headElement.getElementsByClass("author")
-          .text()
-      blog.title = headElement.getElementsByTag("a")
-          .first()
-          .text()
-      blog.url = headElement.getElementsByTag("a")
-          .first()
-          .attr("href")
-      blog.content = bodyElements[i].text()
-          .replace(" ", "")
-      blog.time = bottomElements[i].text().split("｜")[0]
+      val blog = Blog(
+          type = Blog.KEY_NOG,
+          name = headElement.getElementsByClass("author").text(),
+          title = headElement.getElementsByTag("a").first().text(),
+          url = headElement.getElementsByTag("a").first().attr("href"),
+          content = bodyElements[i].text().replace(" ", ""),
+          time = bottomElements[i].text().split("｜")[0]
+      )
       blogList.add(blog)
     }
     return blogList
